dd3f304fccc4c470fe945847a44852a69c548588
Jaybabson007 <yahayajoel2@gmail.com>
2024-05-27 17:43:05 +0100

    Initial Migrations set up

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__
 		)
 app.config.from_object('config.Config')
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def home():
