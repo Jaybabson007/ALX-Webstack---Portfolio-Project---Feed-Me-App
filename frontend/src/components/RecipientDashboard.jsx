8fa1d4415865490a4b1ff334610661146c201f12
Jaybabson007 <yahayajoel2@gmail.com>
2024-05-27 16:00:20 +0100

    frontend added

---
+++ frontend/src/components/RecipientDashboard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Recipient Dashboard Page</h1>
+        </div>
+    );
+};
+
+export default RecipientDashboard;
